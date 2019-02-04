e11c7dadd0398c01ff22665e7ece7b2554ab987b
nicole chung <nicole.chung@gmail.com>
2019-02-04 14:14:08 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+// init project
+const express = require('express');
+const app = express();
+const feeds = require('./lib/fake-feeds')
+const feed = require('./lib/fake-feeds')
+
+var port = process.env.PORT || 8000
+
+app.use(express.static('public'));
+
+app.get('/feeds', function(request, response) {
+  feeds(request, response)
+});
+
+app.get('/feeds/:id', function(request, response) {
+  feed(request, response)
+});
+
+// listen for requests :)
+const listener = app.listen(port, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
